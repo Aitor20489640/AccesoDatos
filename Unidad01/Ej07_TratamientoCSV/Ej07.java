f13a3610bf4ec54c759e860c04b7594a8c5e250c
Aitor Rodriguez <Aitor.Gallardo.14@gmail.com>
2022-10-24 19:51:38 +0200

    Ejercicio 7 W.I.P faltan las vueltas rapidas, los abandonos y los cara a cara

--- Unidad01/Ej07_TratamientoCSV/Ej07.java
+++ Unidad01/Ej07_TratamientoCSV/Ej07.java
@@ -4,26 +4,27 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Ej07 {
     public static void main(String[] args) {
-        List<Carrera> raceResults = new ArrayList<>();
-        List<Carrera> sprintResults = new ArrayList<>();
+        List<CarreraNormal> raceResults = new ArrayList<>();
+        List<CarreraSprint> sprintResults = new ArrayList<>();
         ArrayList<Carrera> allRacesResults = new ArrayList<>();
         Path pathRace = Path.of("Unidad01/Ej07_TratamientoCSV/formula1_2021season_raceResults.csv");
         Path pathSprint = Path.of("Unidad01/Ej07_TratamientoCSV/formula1_2021season_sprintQualifyingResults.csv");
 
         try (Stream<String> lineas = Files.lines(pathRace).skip(1)){
-            raceResults = lineas.map(CarreraNormal::crearCarrera).toList();
+            raceResults = lineas.map(line -> (CarreraNormal) CarreraNormal.crearCarrera(line)).toList();
         } catch (IOException ex) {
             ex.printStackTrace();
         }
 
         try (Stream<String> lineas = Files.lines(pathSprint).skip(1)){
-            sprintResults = lineas.map(CarreraSprint::crearCarrera).toList();
+            sprintResults = lineas.map(line -> (CarreraSprint) CarreraSprint.crearCarrera(line)).toList();
         } catch (IOException ex) {
             ex.printStackTrace();
         }
@@ -33,95 +34,98 @@ public class Ej07 {
 
         System.out.println("-".repeat(20)+"Piloto Campeon"+"-".repeat(20));
 
-        Map<List<String>, Double> mapDriver = allRacesResults.stream()
+        allRacesResults.stream()
                 .collect(Collectors.groupingBy(
                         p -> Arrays.asList(p.getDriver()),
-                        Collectors.summingDouble(Carrera::getPoints)
-                ));
-
-        mapDriver.entrySet()
+                        Collectors.summingDouble(Carrera::getPoints)))
+                .entrySet()
                 .stream()
                 .map(e -> new Driver(e.getKey().get(0), e.getValue())).toList()
                 .stream().sorted((d1, d2) -> Double.compare(d2.value, d1.value)).limit(1).toList()
                 .forEach(System.out::println);
 
 
+
+
         System.out.println("-".repeat(20)+"Equipo Campeon"+"-".repeat(20));
 
-        Map<List<String>, Double> mapTeam = allRacesResults.stream()
+        allRacesResults.stream()
                 .collect(Collectors.groupingBy(
                         p -> Arrays.asList(p.getTeam()),
-                        Collectors.summingDouble(Carrera::getPoints)
-                ));
-
-        mapTeam.entrySet()
+                        Collectors.summingDouble(Carrera::getPoints)))
+                .entrySet()
                 .stream()
                 .map(t -> new Driver(t.getKey().get(0), t.getValue())).toList()
                 .stream().sorted((d1, d2) -> Double.compare(d2.value, d1.value)).limit(1).toList()
                 .forEach(System.out::println);
 
+
+
         System.out.println("-".repeat(15)+"Piloto con mas Victorias"+"-".repeat(15));
 
-        Map<String, Long> countVictoriesD = raceResults.stream().filter(d -> d.getPosition() == 1)
-                .collect(Collectors.groupingBy(Carrera::getDriver, Collectors.counting()));
-
-        countVictoriesD.entrySet()
+        raceResults.stream().filter(d -> d.getPosition() == 1)
+                .collect(Collectors.groupingBy(Carrera::getDriver, Collectors.counting()))
+                .entrySet()
                 .stream()
                 .map(d -> new Driver(d.getKey(), d.getValue())).toList()
                 .stream().sorted((d1, d2) -> Double.compare(d2.value, d1.value)).limit(1).toList()
                 .forEach(System.out::println);
 
+
+
         System.out.println("-".repeat(15)+"Equipo con mas Victorias"+"-".repeat(15));
 
-        Map<String, Long> countVictoriesT = raceResults.stream().filter(t -> t.getPosition() == 1)
-                .collect(Collectors.groupingBy(Carrera::getTeam, Collectors.counting()));
-
-        countVictoriesT.entrySet()
+        raceResults.stream().filter(t -> t.getPosition() == 1)
+                .collect(Collectors.groupingBy(Carrera::getTeam, Collectors.counting()))
+                .entrySet()
                 .stream()
                 .map(t -> new Driver(t.getKey(), t.getValue())).toList()
                 .stream().sorted((d1, d2) -> Double.compare(d2.value, d1.value)).limit(1).toList()
                 .forEach(System.out::println);
 
 
+
+
         System.out.println("-".repeat(15)+"Piloto con mas Podios"+"-".repeat(15));
 
-        Map<String, Long> countPodiumD = raceResults.stream().filter(d -> d.getPosition() >= 1 && d.getPosition() <= 3)
-                .collect(Collectors.groupingBy(Carrera::getDriver, Collectors.counting()));
-
-        countPodiumD.entrySet()
+        raceResults.stream().filter(d -> d.getPosition() >= 1 && d.getPosition() <= 3)
+                .collect(Collectors.groupingBy(Carrera::getDriver, Collectors.counting()))
+                .entrySet()
                 .stream()
                 .map(d -> new Driver(d.getKey(), d.getValue())).toList()
                 .stream().sorted((d1, d2) -> Double.compare(d2.value, d1.value)).limit(1).toList()
                 .forEach(System.out::println);
+
+
 
         System.out.println("-".repeat(15)+"Equipo con mas Podios"+"-".repeat(15));
 
-        Map<String, Long> countPodiumT = raceResults.stream().filter(d -> d.getPosition() >= 1 && d.getPosition() <= 3)
-                .collect(Collectors.groupingBy(Carrera::getTeam, Collectors.counting()));
-
-        countPodiumT.entrySet()
+        raceResults.stream().filter(d -> d.getPosition() >= 1 && d.getPosition() <= 3)
+                .collect(Collectors.groupingBy(Carrera::getTeam, Collectors.counting()))
+                .entrySet()
                 .stream()
                 .map(d -> new Driver(d.getKey(), d.getValue())).toList()
                 .stream().sorted((d1, d2) -> Double.compare(d2.value, d1.value)).limit(1).toList()
                 .forEach(System.out::println);
+
+
 
         System.out.println("-".repeat(15)+"Piloto con mas Poles"+"-".repeat(15));
 
-        Map<String, Long> countPolesD = raceResults.stream().filter(d -> d.getStartingGrid() == 1)
-                .collect(Collectors.groupingBy(Carrera::getDriver, Collectors.counting()));
-
-        countPolesD.entrySet()
+        raceResults.stream().filter(d -> d.getStartingGrid() == 1)
+                .collect(Collectors.groupingBy(Carrera::getDriver, Collectors.counting()))
+                .entrySet()
                 .stream()
                 .map(d -> new Driver(d.getKey(), d.getValue())).toList()
                 .stream().sorted((d1, d2) -> Double.compare(d2.value, d1.value)).limit(1).toList()
                 .forEach(System.out::println);
 
+
         System.out.println("-".repeat(15)+"Equipo con mas Poles"+"-".repeat(15));
 
-        Map<String, Long> countPolesT = raceResults.stream().filter(d -> d.getStartingGrid() == 1)
-                .collect(Collectors.groupingBy(Carrera::getTeam, Collectors.counting()));
-
-        countPolesT.entrySet()
+        raceResults.stream().filter(d -> d.getStartingGrid() == 1)
+                .collect(Collectors.groupingBy(Carrera::getTeam, Collectors.counting()))
+                .entrySet()
                 .stream()
                 .map(d -> new Driver(d.getKey(), d.getValue())).toList()
                 .stream().sorted((d1, d2) -> Double.compare(d2.value, d1.value)).limit(1).toList()
@@ -129,7 +133,11 @@ public class Ej07 {
 
         System.out.println("-".repeat(15)+"Piloto con mas Vueltas Rapidas"+"-".repeat(15));
 
-        List<CarreraNormal> raceResultsLaps = new ArrayList<>((Collection) raceResults);
+
+
+
+
+
 
 
 
@@ -141,6 +149,8 @@ public class Ej07 {
 
 
     }
+
+
 
     private static class Driver {
         protected String nombre;
@@ -159,21 +169,37 @@ public class Ej07 {
     }
 
     private static class FastestLap{
+        protected String track;
         protected String nombre;
-        protected LocalTime lap;
+        protected int[] lap;
 
-        public FastestLap(String nombre, String lap) {
+        public FastestLap(String track, String nombre, String lap) {
+            this.track = track;
             this.nombre = nombre;
-            this.lap = LocalTime.parse(lap);
+            if (lap.equals("N/A")){
+                this.lap = null;
+            }else {
+                this.lap = formatLaps(lap);
+            }
+
         }
 
         @Override
         public String toString() {
-            return "nombre='" + nombre + '\'' +
-                    ", lap='" + lap;
+            return "track='" + track + '\'' +
+                    ", nombre='" + nombre + '\'' +
+                    ", lap=" + Arrays.toString(lap);
         }
-        public boolean compare(LocalTime t1, LocalTime t2) {
-            return t1.isBefore(t2);
+
+        public boolean compare(int[] t1, int[] t2) {
+            return Arrays.compare(t1, t2) == -1;
+        }
+
+        public static int[] formatLaps(String lap) {
+            int minutes = Integer.parseInt(lap.split(":")[0]);
+            int seconds = Integer.parseInt(lap.split(":")[1].split("\\.")[0]);
+            int miliSeconds = Integer.parseInt(lap.split(":")[1].split("\\.")[1]);
+            return new int[]{minutes, seconds, miliSeconds};
         }
     }
 
